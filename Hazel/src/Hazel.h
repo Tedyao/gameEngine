03459c17c214499ca6b814b5e0994df314713494
Tedyao <43025170+Tedyao@users.noreply.github.com>
2022-05-22 15:31:14 +0800

    add entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//for used by Hazel application only
+#include "Hazel/Application.h"
+
+
+
+
+// EntryPoint ------------------------
+#include "Hazel/EntryPoint.h"
+// EntryPoint ------------------------
+
